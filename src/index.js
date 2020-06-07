0bc3beaddfc5bc2fe06f9214e1559e8f8b24cf92
brisdalen <bwendelborgrisdalen@gmail.com>
2020-06-07 12:44:04 +0200

    Working on adding the columns and rows to the history list.
    All extra challenges completed. Should add more comments tho.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 /*
-Display the location for each move in the format (col, row) in the move history list.
+X Display the location for each move in the format (col, row) in the move history list.
 Bold the currently selected item in the move list.
-Rewrite Board to use two loops to make the squares instead of hardcoding them.
+X Rewrite Board to use two loops to make the squares instead of hardcoding them.
 X Add a toggle button that lets you sort the moves in either ascending or descending order.
 When someone wins, highlight the three squares that caused the win.
 X When no one wins, display a message about the result being a draw.
@@ -13,7 +13,10 @@ X When no one wins, display a message about the result being a draw.
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button 
+      className={"square " + (props.isWinning ? "square--winning" : null)}
+      onClick={props.onClick}
+    >
       {props.value}
     </button>
   );
@@ -23,8 +26,12 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
             <Square 
+              isWinning = {this.props.winningSquares.includes(i)}
               value={this.props.squares[i]}
               onClick={() => this.props.onClick(i)}
+              // key={} is to give each child a unique key
+              // Here 's' is for Square
+              key={'s'+i}
             />
     );
   }
@@ -38,6 +45,8 @@ class Board extends React.Component {
         columns.push(this.renderSquare(counter));
         counter++;
       }
+      // key={} is to give each child a unique key
+      // Here 'r' is for row
       rows.push(<div className="board-row" key={'r'+i}>{columns}</div>);
     }
     return (
@@ -54,6 +63,8 @@ class Game extends React.Component {
     this.state = {
       history: [{
         squares: Array(9).fill(null),
+        // The earlier selected moves must also be stored in the history
+        moves: null,
       }],
       stepNumber: 0,
       xIsNext: true,
@@ -65,7 +76,7 @@ class Game extends React.Component {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-
+    // Do nothing if there's a winner or the square is already filled
     if(calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -75,6 +86,8 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{
         squares: squares,
+        // Include the selected sqaure-index in the history's state
+        moves: i,
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
@@ -92,27 +105,33 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
-    
-    const moves = history.map((step, move) => {
-      const desc = move ? 
-        'Go to move #' + move :
-        'Go to game start';
-
-      return (
-        <li>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-       );
-    });
 
     let status;
+    let winLine;
     if(winner) {
-      status = 'Winner: ' + winner;
+      status = 'Winner: ' + winner.player;
+      winLine = winner.line;
     } else if(this.state.stepNumber === 9) {
       status = 'It\'s a draw!'; 
     }else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
+    
+    const moves = history.map((step, move) => {
+      const desc = move ? 
+        // Retrieve the move from step, and calculate the col and row using this
+        'Go to move #' + move + " (" + (step.moves % 3 + 1) + "," + (Math.floor(step.moves / 3) + 1) + ")" 
+        : 'Go to game start';
+      return (
+        // key={} is to give each child a unique key
+        // here I use the move as the unique key
+        <li className="historyElement" key={move}>
+          <button onClick={() => this.jumpTo(move)}>
+            {move == this.state.stepNumber ? <b>{desc}</b> : desc}
+            </button>
+        </li>
+       );
+    });
 
     let orderedHistory;
     if(this.state.descendingHistory) {
@@ -125,6 +144,7 @@ class Game extends React.Component {
       <div className="game">
         <div className="game-board">
           <Board 
+            winningSquares = {winner ? winner.line : []}
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
@@ -132,7 +152,11 @@ class Game extends React.Component {
         <div className="game-info">
           <div>{status}</div>
           <button onClick={() => {
-            this.state.descendingHistory = !this.state.descendingHistory;
+            // Using setState fixed my earlier problem of the rendering of
+            // the history list not updating till the next move was performed
+            this.setState ({
+              descendingHistory: !this.state.descendingHistory,
+            });
             orderedHistory.reverse();
           }}>Reverse move history order</button>
           <ol id="history_list">{orderedHistory}</ol>
@@ -156,7 +180,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {player: squares[a], line: [a, b, c]};
     }
   }
   return null;
